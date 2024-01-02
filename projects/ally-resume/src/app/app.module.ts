e8dc90108e009802f17382f077170fbb25e1f22a
mpowers652 <74209618+mpowers652@users.noreply.github.com>
2024-01-01 17:06:50 -0700

    removalofUnnecessary

--- projects/ally-resume/src/app/app.module.ts
+++ projects/ally-resume/src/app/app.module.ts
@@ -6,15 +6,13 @@ import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
 import { JobComponent } from './job/job.component';
 import { Summary1Component } from './summary1/summary1.component';
-import { Summary2Component } from './summary2/summary2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileComponent,
     JobComponent,
-    Summary1Component,
-    Summary2Component
+    Summary1Component
   ],
   imports: [
     BrowserModule,
