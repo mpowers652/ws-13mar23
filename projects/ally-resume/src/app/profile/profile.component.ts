bcb2c2156b52106e9c0c91a99c3b25f0f57642a0
mpowers652 <74209618+mpowers652@users.noreply.github.com>
2024-01-01 17:00:39 -0700

    profileUpdate

--- projects/ally-resume/src/app/profile/profile.component.ts
+++ projects/ally-resume/src/app/profile/profile.component.ts
@@ -1,20 +1,10 @@
 import { Component } from '@angular/core';
-import { JobComponent } from '../job/job.component';
 
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
   styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
-  jobList:JobComponent[] = [];
-  majorDuties:Set<string> = new Set();
+export class ProfileComponent{
   
-  addToDuties():void {
-    for (let index = 0; index < this.jobList.length; index++) {
-      const element = this.jobList[index];
-      const duties = element.duties;
-      duties.forEach(this.majorDuties.add);
-    }
-  }
 }
--- projects/ally-resume/src/app/summary1/summary1.component.ts
+++ projects/ally-resume/src/app/summary1/summary1.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { JobComponent } from '../job/job.component';
 
 @Component({
   selector: 'app-summary1',
   templateUrl: './summary1.component.html',
   styleUrls: ['./summary1.component.css']
 })
-export class Summary1Component {
+export class Summary1Component implements OnInit{
+  jobList:JobComponent[] = [];
+  majorDuties:Set<string> = new Set();
+  
+  ngOnInit():void{this.addToDuties}
 
+  addToDuties():void {
+    for (let index = 0; index < this.jobList.length; index++) {
+      const element = this.jobList[index];
+      const duties = element.duties;
+      duties.forEach(this.majorDuties.add);
+    }
+  }
 }
